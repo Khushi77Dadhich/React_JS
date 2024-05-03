45995875abd3a2e5f1d20a1d9a7afe67c2e33455
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2024-05-03 00:50:15 +0000

    about hooks

---
+++ hooks/src/destruct.jsx
@@ -0,0 +1,12 @@
+import animals from "./animal";
+
+// //destructing an array
+// console.log(animals);
+// const [cat, dog] = animals;
+// console.log(cat);
+
+//desturcting objects
+const {name, sound} = cat;
+const {name: catName, sound: catsound} = cat;
+const {feedingRequirement: {food, water}} = cat;
+console.log(food);
