adf3f9fbf2bc1f8a0ec046b06e0d2fc9747a04bf
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2024-07-01 03:18:25 +0000

    learning react

--- learning-1/src/index.js
+++ learning-1/src/index.js
@@ -1,14 +1,20 @@
-import App from './App';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
 
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement);
+// const rootElement = document.getElementById('root')
+// const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
 
 
+import ReactDOM from "react-dom/client";
+import App from "./App"
+import "./index.css";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App/>);
